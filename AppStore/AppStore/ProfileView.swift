0ebfca443362c022df716cc36bff87bf0ab5f0fb
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-08 08:30:43 +0900

    feat: ProfileView 생성 및 TodayView 날짜변경시 뷰에 업데이트 #4

---
+++ AppStore/AppStore/ProfileView.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileView.swift
+//  AppStore
+//
+//  Created by yudonlee on 2022/06/07.
+//
+
+import SwiftUI
+
+let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "M월 d일 EEEE"
+    formatter.locale = Locale(identifier: "ko_kr")
+    return formatter
+}()
+
+struct CurrentDateView: View {
+    @State var todayDate: Date = Date()
+    var body: some View{
+        HStack {
+            Text("\(dateFormatter.string(from: todayDate))")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            Spacer()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.significantTimeChangeNotification)
+        ) { _ in todayDate = Date() }
+    }
+}
+struct ProfileView: View {
+    var selectedTabview: TabViewItemType
+    var body: some View {
+        VStack {
+            if selectedTabview == .today {
+                CurrentDateView()
+            }
+            HStack {
+                Text("\(selectedTabview.text)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+                Image(systemName: "person.crop.circle")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(selectedTabview: .today)
+    }
+}
+
