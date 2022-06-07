c7d0c33dca08f4ddb3ca9f24d7048af22a43b99f
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-07 19:46:41 +0900

    feat: 빈 프로젝트 생성

---
+++ AppStore/AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by yudonlee on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
