356cf970f6ea3d87a535298cb6260a52da50a2e2
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-07 19:56:11 +0900

    feat: 메인 탭뷰 생성 및 하위뷰 생성#3

---
+++ AppStore/AppStore/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  AppStore
+//
+//  Created by yudonlee on 2022/06/07.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
--- AppStore/AppStore/MainTabView.swift
+++ AppStore/AppStore/MainTabView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainTabView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello World!")
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainTabView()
     }
 }
