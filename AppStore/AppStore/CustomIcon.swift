f5bced35139c2e9bd3e1d040b3be7528a0b759cc
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-07 20:52:36 +0900

    feat: Tabview 추가 및 TabViewItem enum으로 관리 #3

---
+++ AppStore/AppStore/CustomIcon.swift
@@ -0,0 +1,28 @@
+//
+//  CustomIcon.swift
+//  AppStore
+//
+//  Created by yudonlee on 2022/06/07.
+//
+
+import SwiftUI
+
+enum TabViewItemType: String {
+    case today = "투데이"
+    case game
+    case app = "앱"
+    case arcade
+    case search = "검색"
+    var image: Image {
+        switch self {
+        case .today: return Image(systemName: "doc.text.image")
+        case .game: return Image("GameIcon")
+        case .app: return Image(systemName: "square.stack.3d.up.fill")
+        case .arcade: return Image("ArcadeIcon")
+        case .search: return Image(systemName: "magnifyingglass")
+        }
+    }
+    var text: Text {
+        Text(self.rawValue)
+    }
+}
--- AppStore/AppStore/MainTabView.swift
+++ AppStore/AppStore/MainTabView.swift
@@ -9,11 +9,41 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text("Hello World!")
-            .padding()
+        TabView {
+            TodayView()
+                .tabItem {
+                    TabViewItem(type: .today)
+                }
+            GameView()
+                .tabItem {
+                    TabViewItem(type: .game)
+                }
+            AppView()
+                .tabItem {
+                    TabViewItem(type: .app)
+                }
+            TodayView()
+                .tabItem {
+                    TabViewItem(type: .arcade)
+                }
+            TodayView()
+                .tabItem {
+                    TabViewItem(type: .search)
+                }
+        }
+        .font(.headline)
     }
 }
-
+struct TabViewItem: View {
+    var type: TabViewItemType
+    var body: some View {
+        VStack {
+            type.image
+                .renderingMode(.template)
+            type.text
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
